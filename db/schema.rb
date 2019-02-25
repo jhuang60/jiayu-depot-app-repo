0ff6f29649b078d35b04de35c243eab4a7928b1f
jhuang60 <jhuang60@hawk.iit.edu>
2019-02-24 23:39:42 -0600

    finish chapter 10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_045904) do
+ActiveRecord::Schema.define(version: 2019_02_25_050526) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
