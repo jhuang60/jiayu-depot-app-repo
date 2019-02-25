75b3baf89140471327e855f58f78b2f786495c86
jhuang60 <jhuang60@hawk.iit.edu>
2019-02-24 22:05:46 -0600

    scaffold generated and current_cart.rb modified
    rails generate scaffold Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_10_013217) do
+ActiveRecord::Schema.define(version: 2019_02_25_035902) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
