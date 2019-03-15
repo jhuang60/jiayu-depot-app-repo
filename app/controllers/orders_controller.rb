1ab90aeb561b21fcd10f584dfa0e32cd249e5d98
jhuang60 <jhuang60@hawk.iit.edu>
2019-03-15 00:17:33 -0500

    capturing an order 12.1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,11 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
 
+  include CurrentCart
+  before_action :set_cart, only: [:new, :create]
+  before_action :ensure_cart_isnt_empty, only: :new
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+
   # GET /orders
   # GET /orders.json
   def index
@@ -24,18 +29,24 @@ class OrdersController < ApplicationController
   # POST /orders
   # POST /orders.json
   def create
-    @order = Order.new(order_params)
+      @order = Order.new(order_params)
+      @order.add_line_items_from_cart(@cart)
 
-    respond_to do |format|
-      if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
-        format.json { render :show, status: :created, location: @order }
-      else
-        format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @order.save
+          Cart.destroy(session[:cart_id])
+          session[:cart_id] = nil
+          format.html { redirect_to store_index_url, notice:
+            'Thank you for your order.' }
+          format.json { render :show, status: :created,
+            location: @order }
+        else
+          format.html { render :new }
+          format.json { render json: @order.errors,
+            status: :unprocessable_entity }
+        end
       end
     end
-  end
 
   # PATCH/PUT /orders/1
   # PATCH/PUT /orders/1.json
@@ -71,4 +82,12 @@ class OrdersController < ApplicationController
     def order_params
       params.require(:order).permit(:name, :address, :email, : pay_type)
     end
+
+    private
+   def ensure_cart_isnt_empty
+     if @cart.line_items.empty?
+       redirect_to store_index_url, notice: 'Your cart is empty'
+     end
+   end
+
 end
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -10,14 +10,21 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "requires item in cart" do
+    get new_order_url
+    assert_redirected_to store_index_path
+    assert_equal flash[:notice], 'Your cart is empty'
+  end
+
   test "should get new" do
+    post line_items_url, params: { product_id: products(:ruby).id }
     get new_order_url
     assert_response :success
   end
 
   test "should create order" do
     assert_difference('Order.count') do
-      post orders_url, params: { order: {  pay_type: @order. pay_type, address: @order.address, email: @order.email, name: @order.name } }
+      post orders_url, params: { order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type } }
     end
 
     assert_redirected_to order_url(Order.last)
